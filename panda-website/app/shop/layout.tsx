8782a716d3b9b2f29368f71620db084c3d714785
baiwusanyu-c <chenzhihui@chabaidao.com>
2024-12-05 19:05:24 +0800

    feat: 各个模块 layout 更新 footer组件爱你

--- panda-website/app/shop/layout.tsx
+++ panda-website/app/shop/layout.tsx
@@ -1,6 +1,7 @@
 import { PandaFooter } from "@/components/footer";
 import { TopBlock } from "@/components/top-block";
 import { Wave } from "@/components/wave";
+import type {BasicInfo} from "@/app/api/route";
 
 // TODO: 获取 Footer 数据
 export default async function RootLayout({
@@ -8,11 +9,16 @@ children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  async function getData(){
+    const res = await fetch('http://localhost:3000/api', { method: 'post' });
+    return await res.json()
+  }
+  const res: { data: BasicInfo } = await getData()
   return (
     <div className='w-full h-full overflow-y-auto pf left-0 top-0 z-[1]'>
       <TopBlock/>
       {children}
-      <PandaFooter>
+      <PandaFooter data={res.data}>
         <Wave/>
       </PandaFooter>
     </div>
