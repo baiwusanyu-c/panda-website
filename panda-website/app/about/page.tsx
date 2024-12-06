0f36deb3f216b20d3c9deaf1e3b6e091b277ca71
baiwusanyu-c <chenzhihui@chabaidao.com>
2024-12-06 19:26:26 +0800

    chore: 临时提交

--- panda-website/app/about/page.tsx
+++ panda-website/app/about/page.tsx
@@ -1,17 +1,20 @@
+
+import ShopCarousel from '@/components/shop-carousel'
 // TODO: 接口返回
 export default function AboutPage() {
   return (
       <div className="chanpanda-about h-[100vh] w-full">
-          AboutPage
-          <div>
+        <div className='w-full fcc flex-col pt-[80px]'>
+          <h1 className='text-[44px] text-cbd-brand-5 leading-[1.5] font-bold'>Image Display</h1>
+          <h2 className='text-[32px] text-cbd-gray-6'>形象展示</h2>
+          <ShopCarousel/>
+        </div>
+        <div>
               <h2></h2>
-          </div>
-          <div>
-              <h2></h2>
-          </div>
-          <div>
-              <h2></h2>
-          </div>
+        </div>
+        <div>
+            <h2></h2>
+        </div>
       </div>
   );
 }
--- panda-website/components/shop-carousel/index.tsx
+++ panda-website/components/shop-carousel/index.tsx
@@ -1,39 +1,65 @@
+'use client'
 import React, { useRef, useState } from 'react';
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 // Import Swiper styles
 import 'swiper/css';
+import 'swiper/css/effect-coverflow';
 import 'swiper/css/pagination';
 
-import './styles.css';
+import './style.css';
 
 // import required modules
-import { Pagination } from 'swiper/modules';
+import { EffectCoverflow, Pagination, Autoplay } from 'swiper/modules';
 
 export default function App() {
   return (
-    <>
+    <div className="pr h-full w-[98vw]">
       <Swiper
-        slidesPerView={'auto'}
+        effect={'coverflow'}
+        grabCursor={true}
         centeredSlides={true}
         spaceBetween={30}
-        pagination={{
-          clickable: true,
+        loop={true}
+        // autoplay={{
+        //   delay: 2500,
+        //   disableOnInteraction: false,
+        // }}
+        slidesPerView={'auto'}
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
         }}
-        modules={[Pagination]}
-        className="mySwiper"
+        pagination={{
+          bulletActiveClass: '!bg-cbd-brand-5',
+          horizontalClass: 'swiper-pagination-horizontal',
+          clickable: true
+        }}
+        modules={[Autoplay, EffectCoverflow, Pagination]}
       >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
+        <SwiperSlide  className='w-[925px]'>
+          <img src="https://swiperjs.com/demos/images/nature-1.jpg"  className='w-[925px] h-[618px]'/>
+        </SwiperSlide>
+        <SwiperSlide className='w-[925px]'>
+          <img src="https://swiperjs.com/demos/images/nature-2.jpg"  className='w-[925px] h-[618px]'/>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-3.jpg"   className='w-[925px] h-[618px]'/>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-4.jpg"  className='w-[925px] h-[618px]'/>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-5.jpg"  className='w-[925px] h-[618px]'/>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-6.jpg"  className='w-[925px] h-[618px]'/>
+        </SwiperSlide>
       </Swiper>
-    </>
+    </div>
   );
 }
